64db9d55d105123c22ce4cb1f81b9dab4f7f82f9
Akmal Abbasov <akmal.abbasov@hivestreaming.com>
2017-04-24 22:55:24 +0200

    Better solution for Symmetric Order problem

--- symmetric_order/solution.cpp
+++ symmetric_order/solution.cpp
@@ -11,15 +11,17 @@ int main() {
       break;
 
     string a[n];
-    for (int i = 0; i < n; ++i) {
-      cin >> a[i];
+    for (int i = 0, j = 0; i < n; ++i) {
+      if (i%2) {
+        cin >> a[n-j];
+        
+      } else {
+        cin >> a[j++];
+      }
     }
 
     cout << "SET " << k << endl;
-    for (int i = 0; i < n; i += 2)
-      cout << a[i] << endl;
-
-    for (int i = n - 1 - n%2; i > 0; i -= 2)
+    for (int i = 0; i < n; ++i)
       cout << a[i] << endl;
   }
 
